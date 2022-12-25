b8648db9e7857d4a0f1fd8c59f43ebde1ed4f775
Murat Davletbakov <goodofdmm@gmail.com>
2022-12-25 10:23:12 +0500

    feat: update create function

--- online-store/src/functions/functions.ts
+++ online-store/src/functions/functions.ts
@@ -1,7 +1,13 @@
-const createElement = (selector: string, name: string, text?: string): HTMLElement => {
+const createElement = (selector: string, name: string, text?: string, attrs?: object): HTMLElement => {
   const element = document.createElement(selector);
   element.className = name;
   if (text) element.textContent = text;
+    if (attrs) {
+      type Key = keyof typeof attrs;
+      for (const key in attrs) {
+        element.setAttribute(key, attrs[key as Key]);
+      }
+    }
   return element;
 }
 
