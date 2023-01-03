5cd3da7ae2374df31a1c4af38df47d8a1b1e1c6b
Murat Davletbakov <goodofdmm@gmail.com>
2023-01-03 17:03:20 +0500

    feat: update returnElement function for inputs

--- online-store/src/functions/functions.ts
+++ online-store/src/functions/functions.ts
@@ -1,7 +1,7 @@
 const returnElement = (selector: string, name: string, text?: string, attrs?: object) => {
   const element = document.createElement(selector);
   element.className = name;
-  if (text) element.textContent = text;
+  if (text && selector !== 'input') element.textContent = text;
   if (attrs) {
     type Key = keyof typeof attrs;
     for (const key in attrs) {
