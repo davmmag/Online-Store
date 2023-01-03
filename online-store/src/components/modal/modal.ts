2eca9942f797d619c41466692d0e29394583ea18
Murat Davletbakov <goodofdmm@gmail.com>
2023-01-03 15:57:31 +0500

    feat: renamed function for create elements

--- online-store/src/components/modal/modal.ts
+++ online-store/src/components/modal/modal.ts
@@ -1,17 +1,17 @@
-import { createElement } from '../../functions/functions';
+import { returnElement } from '../../functions/functions';
 class Modal {
   modal: HTMLElement;
   constructor() {
-    this.modal = createElement('div', 'modal visually-hidden');
+    this.modal = returnElement('div', 'body', 'modal visually-hidden');
   }
 
-  draw(data: HTMLElement = createElement('div', 'hello')): void {
-    const container = createElement('div', 'modal__container') as HTMLElement;
-    const body = createElement('div', 'modal__body') as HTMLElement;
-    const btnClose = createElement('button', 'btn modal__close') as HTMLElement;
+  draw(data: HTMLElement): void {
+    const container = returnElement('div', 'modal__container');
+    const body = returnElement('div', 'modal__body') as HTMLElement;
+    const btnClose = returnElement('button', 'btn modal__close') as HTMLElement;
     btnClose.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     btnClose.addEventListener('click', Modal.switchModal);
-    const content = createElement('div', 'modal__content') as HTMLElement;
+    const content = returnElement('div', 'modal__content') as HTMLElement;
     content.append(data);
     body.append(btnClose, content);
     container.append(body);
--- online-store/src/functions/functions.ts
+++ online-store/src/functions/functions.ts
@@ -1,14 +1,14 @@
-const createElement = (selector: string, name: string, text?: string, attrs?: object): HTMLElement => {
+const returnElement = (selector: string, name: string, text?: string, attrs?: object) => {
   const element = document.createElement(selector);
   element.className = name;
   if (text) element.textContent = text;
-    if (attrs) {
-      type Key = keyof typeof attrs;
-      for (const key in attrs) {
-        element.setAttribute(key, attrs[key as Key]);
-      }
+  if (attrs) {
+    type Key = keyof typeof attrs;
+    for (const key in attrs) {
+      element.setAttribute(key, attrs[key as Key]);
     }
+  }
   return element;
 }
 
-export { createElement };
+export { returnElement };
